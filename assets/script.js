973739b7456232f1b914396e289f06ff8d9680aa
hugo566 <hugo56.lg@gmail.com>
2023-11-06 16:01:59 +0100

    realisation du changement d'image

--- assets/script.js
+++ assets/script.js
@@ -17,68 +17,31 @@ const slides = [
 	}
 ]
 
-const arrowLeft = document.querySelector("arrow_left");
-const arrowRight = document.querySelector("arrow_right");
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
 const img = document.getElementById("bg-img");
 
-// arrowLeft.addEventListener("click", () => {
-// 	for (let i = 0; i < slides; i++) {
-// 		if (img.src == "./assets/images/slideshow/" + slides[1].image) {
-// 			img.src = "./assets/images/slideshow/" + slides[2].image;
-// 		} else {
-// 			img.src = "./assets/images/slideshow/" + slides[2].image;
-// 		}
-// 	}
-// })
+let currentIndex = 0;
 
-function changeImg() {
-	for (let i = 0; i < slides.length; i++) {
-		if (img.src == "./assets/images/slideshow/" + slides[i].image) {
-			img.src = "./assets/images/slideshow/" + slides[i].image;
-		} else {
-			img.src = "./assets/images/slideshow/" + slides[i].image;
-		}
+function changeImg(index, direction) {
+	if (currentIndex === -1 && direction === "left") {
+		currentIndex = slides.length - 1;
+	} else if (currentIndex === slides.length && direction === "right") {
+		currentIndex = 0;
 	}
+
+	const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
+	img.src = imagePath;
+	img.alt = `Slide ${currentIndex + 1}`;
 }
 
+arrowLeft.addEventListener('click', () => {
+	currentIndex = (currentIndex - 1);
+	changeImg(currentIndex, "left");
+});
 
 
-// function changeImg() {
-// 	if (img.src == "./assets/images/slideshow/slide1.jpg") {
-// 		img.src = "./assets/images/slideshow/slide2.jpg";
-
-// 		if (img.src == "./assets/images/slideshow/slide2.jpg") {
-// 			img.src = "./assets/images/slideshow/slide3.jpg";
-
-// 			if (img.src == "./assets/images/slideshow/slide3.jpg") {
-// 				img.src = "./assets/images/slideshow/slide4.png";
-
-// 				if (img.src == "./assets/images/slideshow/slide4.png") {
-// 					img.src = "./assets/images/slideshow/slide1.jpg";
-// 				} else {
-// 					img.src = "./assets/images/slideshow/slide1.jpg";
-// 				}
-// 			} else {
-// 				img.src = "./assets/images/slideshow/slide4.png";
-// 			}
-// 		} else {
-// 			img.src = "./assets/images/slideshow/slide3.jpg";
-// 		}
-// 	} else {
-// 		img.src = "./assets/images/slideshow/slide2.jpg";
-// 	}
-// }
-
-
-// function changeImg() {
-// 	if (img.src == "./assets/images/slideshow/slide1.jpg") {
-// 		img.src = "./assets/images/slideshow/slide2.jpg";
-// 	} else if (img.src = "./assets/images/slideshow/slide2.jpg") {
-// 		img.src = "./assets/images/slideshow/slide3.jpg";
-// 	} else if (img.src = "./assets/images/slideshow/slide3.jpg") {
-// 		img.src = "./assets/images/slideshow/slide4.png";
-// 	} else {
-// 		img.src = "./assets/images/slideshow/slide1.jpg";
-// 	}
-
-// }
+arrowRight.addEventListener('click', () => {
+	currentIndex = (currentIndex + 1);
+	changeImg(currentIndex, "right");
+});
